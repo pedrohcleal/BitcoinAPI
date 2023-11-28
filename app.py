90976c96b4f1b96a1ed332783aa2dd5e73db460a
Pedro Leal <pedroleal360@gmail.com>
2023-11-28 19:13:39 -0300

    subir-final-requerimento4

--- app.py
+++ app.py
@@ -38,15 +38,19 @@ def send_btc():
     info_utxo = requestp1.escolher_utxos(endereco_destino)
     print("solicitação antedida")
     if (info_utxo):
-        
         return render_template('detalhes_template.html', json_response=info_utxo)
     else:
         return jsonify({"erro": "Não foi possível obter os detalhes do endereço"}), 500
-    return jsonify(data)
 
-@app.route('/txt/<txt>')  #https://api.blockcypher.com/v1/btc/main/txs
-def pagina5(txt):
-    return f'Olá, {txt}!'
+@app.route('/tx/<tx>')  #https://api.blockcypher.com/v1/btc/main/txs
+def pagina5(tx):
+    info_txn = requestp1.info_tx(tx)
+
+    if (info_txn):
+        return render_template('detalhes_template.html', json_response=info_txn)
+    else:
+        return jsonify({"erro": "Não foi possível obter os detalhes do endereço"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- requestp1.py
+++ requestp1.py
@@ -2,16 +2,11 @@ import requests
 import json
 import time
 
-address = "bc1qyzxdu4px4jy8gwhcj82zpv7qzhvc0fvumgnh0r"
-url = "https://api.blockcypher.com/v1/btc/main/addrs/{}".format(address)
-response = requests.get(url)
-
-
 def get_bitcoin_address_info(address):
+    url = "https://api.blockcypher.com/v1/btc/main/addrs/{}".format(address)
+    response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-
-        # Criar um dicionário combinando os dados
         resultado_json = (
             '{\n'
             f'    "address": "{data["address"]}",\n'
@@ -27,58 +22,66 @@ def get_bitcoin_address_info(address):
             '    }\n'
             '}\n'
         )
-        #print(resultado_json)
-        
-        return json.dumps(data,indent=2)
+        return resultado_json
     else:
-        return response.status_code
+        return "O endereço de bitcoin está inválido : Erro {}".format(response.status_code)
 
 def balance(address):
+    url = "https://api.blockcypher.com/v1/btc/main/addrs/{}".format(address)
+    response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
 
-        # Criar um dicionário combinando os dados
         resultado_json = (
             '{\n'
             f'  "confirmed": "{data["final_balance"]}",\n'
             f'  "unconfirmed": "{data["unconfirmed_balance"]}"\n'
             '}\n'
         )
-        #print(resultado_json)
         
         return resultado_json
     else:
-        return response.status_code
+        return "O endereço de bitcoin está inválido : Erro {}".format(response.status_code)
     
 def escolher_utxos(address):
+    url = "https://api.blockcypher.com/v1/btc/main/addrs/{}".format(address)
+    response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         utxos = []
-        # Criar um dicionário combinando os dados
+
         resultado_json = data["txrefs"]
         for indice, tx in enumerate(data["txrefs"]):
-            
-            utxos.append({"txid":resultado_json[indice]["tx_hash"],"amount":resultado_json[indice]["value"]})
-            #utxos.append({"amount":resultado_json[indice]["value"]})
-        
+            utxos.append({"txid":resultado_json[indice]["tx_hash"],
+                          "amount":resultado_json[indice]["value"]})
+
         dict_utxo = {"utxos":utxos}
         return json.dumps(dict_utxo,indent=2)
     else:
-        return response.status_code
+        return "O endereço de bitcoin está inválido : Erro {}".format(response.status_code)
     
 def info_tx(t_id):
     url = "https://api.blockcypher.com/v1/btc/main/txs/{}".format(t_id)
     response = requests.get(url)
-
     if response.status_code == 200:
         data = response.json()
-        return json.dumps(data,indent=2)
+        data_outputs= data["outputs"]
+        print(data_outputs)
+        list_address = []
+
+        for indice, tx in enumerate(data_outputs):
+            list_address.append({"address":data_outputs[indice]["addresses"][0],
+                                 "value":data_outputs[indice]["value"]})
+        
+        dict_tx = {"addresses": list_address, 
+                   "block": data["block_height"],
+                   "txID": data["hash"] }
+        
+        return json.dumps(dict_tx,indent=2)
     else:
-        return response.status_code
+        return "ID de transação está inválido : Erro {}".format(response.status_code)
 
-
-
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     address = "de6d495ede6dbb072861ec38609dd065182ef784e1bc20bc7c1df651f404b0f7"
     info = info_tx(address)
-    print(info)
+    print(info)"""
